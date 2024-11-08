201d0c8aa8155ed49c681f9c2c921709c8068af7
Eric Neves <ericnevesrodrigues@gmail.com>
2024-11-08 14:46:36 -0400

    feat: enable cors

--- www/index.php
+++ www/index.php
@@ -4,6 +4,7 @@ error_reporting(0);
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config/bootstrap.php';
+require_once __DIR__ . '/config/cors.php';
 require_once __DIR__ . '/routes/api.php';
 
 $middlewares = require_once __DIR__ . '/config/middlewares.php';
@@ -17,5 +18,7 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 date_default_timezone_set('America/Sao_Paulo');
+
+allowCors();
 
 dispatch(Route::getRoutes(), $middlewares);
